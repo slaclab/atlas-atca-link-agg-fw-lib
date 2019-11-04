b10628ed40863505d27b419ebc6e10f74f31ec62
Larry Ruckman <ruckman@slac.stanford.edu>
2019-11-04 15:13:27 -0800

    adding .xdc files

--- ruckus.tcl
+++ ruckus.tcl
@@ -12,5 +12,15 @@ loadConstraints -path "$::DIR_PATH/xdc/AtlasAtcaLinkAggTiming.xdc"
 loadIpCore -path "$::DIR_PATH/ip/SysMonCore.xci"
 loadIpCore -path "$::DIR_PATH/ip/LvdsSgmiiEthPhy.xci"
 
+loadConstraints -path "$::DIR_PATH/ip/LvdsSgmiiEthPhy_clocks.xdc"
+set_property PROCESSING_ORDER {EARLY}           [get_files {LvdsSgmiiEthPhy_clocks.xdc}]
+set_property SCOPED_TO_REF    {LvdsSgmiiEthPhy} [get_files {LvdsSgmiiEthPhy_clocks.xdc}]
+set_property SCOPED_TO_CELLS  {inst}            [get_files {LvdsSgmiiEthPhy_clocks.xdc}]
+
+loadConstraints -path "$::DIR_PATH/ip/LvdsSgmiiEthPhy.xdc"
+set_property PROCESSING_ORDER {LATE}            [get_files {LvdsSgmiiEthPhy.xdc}]
+set_property SCOPED_TO_REF    {LvdsSgmiiEthPhy} [get_files {LvdsSgmiiEthPhy.xdc}]
+set_property SCOPED_TO_CELLS  {inst}            [get_files {LvdsSgmiiEthPhy.xdc}]
+
 # Adding the common Si5345 configuration
 add_files -norecurse "$::DIR_PATH/pll-config/AtlasAtcaLinkAggDefaultPllConfig.mem"
