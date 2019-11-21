3ce56af2c38aa0b217f828f99d86ded0fa9b65cd
Larry Ruckman <ruckman@slac.stanford.edu>
2019-11-21 08:23:44 -0800

    Update ruckus.tcl

--- ruckus.tcl
+++ ruckus.tcl
@@ -1,5 +1,16 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
+# Check for submodule tagging
+if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
+   if { [SubmoduleCheck {ruckus} {2.0.1}  ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}   {2.0.0} ] < 0 } {exit -1}
+} else {
+   puts "\n\n*********************************************************"
+   puts "OVERRIDE_SUBMODULE_LOCKS != 0"
+   puts "Ignoring the submodule locks in axi-pcie-core/ruckus.tcl"
+   puts "*********************************************************\n\n"
+}
+
 # Check for version 2018.3 of Vivado (or later)
 if { [VersionCheck 2018.3] < 0 } {exit -1}
 
